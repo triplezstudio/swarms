762abf05881ecf4625a1f7c3bb96f9e1a8eb7d9c
Martin Gruscher <martingruscher@gmail.com>
2026-04-28 17:25:06 +0200

    test transforms

--- src/client/main.cc
+++ src/client/main.cc
@@ -28,9 +28,10 @@ void doFrame(tz::App* app)
     app->renderQuad({Eigen::Vector3f(0 + i * 1.2, 0, -10)});
   }
 
-  app->renderCube({Eigen::Vector3f(0, 0, 0), Eigen::Vector3f(1, 2, 1)});
-  for (int i = 0; i < 10; i++) {
-    for (int z = 0; z < 10; z++) {
+  app->renderCube({Eigen::Vector3f(.5, 0, 0), Eigen::Vector3f(1, 1, 1)});
+   app->renderCube({Eigen::Vector3f(1.5, 0, -1.5), Eigen::Vector3f(1, 1, 1)});
+  for (int i = 0; i < 5; i++) {
+    for (int z = 0; z < 5; z++) {
        app->renderCube({Eigen::Vector3f(-5 + i * 1.5, 0, -5 + z * 1.5), Eigen::Vector3f(.1, .01, .1)});
     }
   
